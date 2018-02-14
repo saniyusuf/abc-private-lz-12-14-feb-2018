bc9070f8bbffe4a0e7d665c6db1693fe303eb4ad
Sani Yusuf <saniyusuf@gmail.com>
2018-02-14 10:06:24 -0800

    step 301

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from './app-routing/app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
@@ -12,7 +12,8 @@ import {AppRoutingModule} from './app-routing/app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
